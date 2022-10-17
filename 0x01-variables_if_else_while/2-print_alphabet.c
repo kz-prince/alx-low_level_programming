c9d763cf5218e3324a4cdb2226c7716734b6b8ab
kz-prince <kzprince1234@gmail.com>
2022-10-17 15:53:37 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-
 /**
  * main - prints alpha in lower case
  *
- * Return: 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
 	putchar("\n");
 	return (0);
 }
