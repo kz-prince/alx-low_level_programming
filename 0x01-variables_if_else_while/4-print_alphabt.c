9484e9b044504a8d591b5b29185954bce09c3ba5
kz-prince <kzprince1234@gmail.com>
2022-10-17 18:51:04 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - program for alphabet
  *
- * Return: Always 0 (sucess)
+ *Return: Always 0 (success)
  */
-
 int main(void)
 {
 	char c;
 
-	c = 'a';
-	while
-		(c <= 'z') {
-			if ((c != 'q' && c != 'e') && c <= 'z')
-				putchar(c);
-			c++;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
 		}
+	}
+
 	putchar('\n');
 	return (0);
+}
