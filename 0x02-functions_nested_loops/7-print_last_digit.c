8a0c4216996791f2047a62da6f5e2edb7879997e
kz-prince <kzprince1234@gmail.com>
2022-10-19 18:03:49 -0700

    task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @n: the number
+ * Return: Value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	if (ld < 0)
+		ls *= -1;
+
+	_putcher(ld + '0");
+	return (0);
+}
+
