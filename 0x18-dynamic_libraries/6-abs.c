f14e047ec218b3090fffc807ac6797f95f82c81c
kz-prince <kzprince1234@gmail.com>
2023-01-16 13:33:01 -0800

    new commit with different approach

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the abs value of an int
+ * @n: the int question
+ * Return: the int question
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -2,12 +2,9 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * @c: char to be printed
+ * Return: 1 on success
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
